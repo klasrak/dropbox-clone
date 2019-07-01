be613c832f0e993691fdb41ae55cb99cd43b3371
Danilo Augusto <dasf_cm@terra.com.br>
2019-06-30 22:25:38 -0300

    feat: add upload progress bar

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,6 +3,9 @@ class DropBoxController {
     this.btnSendFileEl = document.querySelector('#btn-send-file')
     this.inputFilesEl = document.querySelector('#files')
     this.snackModalEl = document.querySelector('#react-snackbar-root')
+    this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
+    this.namefileEl = this.snackModalEl.querySelector('.filename')
+    this.timeleftEl = this.snackModalEl.querySelector('.timeleft')
     this.initEvents()
   }
 
@@ -12,8 +15,13 @@ class DropBoxController {
     })
     this.inputFilesEl.addEventListener('change', event => {
       this.uploadTask(event.target.files)
-      this.snackModalEl.style.display = 'block'
+      this.modalShow()
+      this.inputFilesEl.value = ''
     })
+  }
+
+  modalShow (show = true) {
+    this.snackModalEl.style.display = (show) ? 'block' : 'none'
   }
 
   uploadTask (files) {
@@ -23,6 +31,7 @@ class DropBoxController {
         let ajax = new XMLHttpRequest()
         ajax.open('POST', '/upload')
         ajax.onload = event => {
+          this.modalShow(false)
           try {
             resolve(JSON.parse(ajax.responseText))
           } catch (e) {
@@ -30,13 +39,45 @@ class DropBoxController {
           }
         }
         ajax.onerror = event => {
+          this.modalShow(false)
           reject(event)
+        }
+        ajax.upload.onprogress = event => {
+          this.uploadProgress(event, file)
         }
         let formData = new FormData()
         formData.append('input-file', file)
+        this.startuploadTime = Date.now()
         ajax.send(formData)
       }))
     })
     return Promise.all(promises)
+  }
+
+  uploadProgress (event, file) {
+    let timeSpent = Date.now() - this.startuploadTime
+    let loaded = event.loaded
+    let total = event.total
+    let percent = parseInt((loaded / total) * 100)
+    let timeleft = ((100 - percent) * timeSpent) / percent
+    this.progressBarEl.style.width = `${percent}%`
+    this.namefileEl.innerHTML = file.name
+    this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft)
+  }
+
+  formatTimeToHuman (duration) {
+    let seconds = parseInt((duration / 1000) % 60)
+    let minutes = parseInt((duration / (1000 * 60)) % 60)
+    let hours = parseInt((duration / (1000 * 60 * 60)) % 24)
+    if (hours > 0) {
+      return `${hours} horas, ${minutes} minutos e ${seconds} segundos`
+    }
+    if (minutes > 0) {
+      return `${minutes} minutos e ${seconds} segundos`
+    }
+    if (seconds > 0) {
+      return `${seconds} segundos`
+    }
+    return ''
   }
 }
