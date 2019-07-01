aad6584484338b65848cfad9e934389fcf312964
Danilo Augusto <dasf_cm@terra.com.br>
2019-06-30 21:13:25 -0300

    feat: add formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const formidable = require('formidable')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.post('/upload', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  })
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      files
+    })
+  })
+})
+
+module.exports = router
